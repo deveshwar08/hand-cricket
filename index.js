0b440356ac536e40afd299692f24b848f10f833b
Devesh <deveshwar00@gmail.com>
2021-08-24 19:05:34 +0530

    all features are implemented

--- index.js
+++ index.js
@@ -1,37 +1,116 @@
-const form = document.getElementById('hand-cricket');
-form.addEventListener('submit', e => {
+const roles = ['Batting', 'Bowling'];
+let userRole = '';
+let userScore = 0;
+let computerRole = '';
+let computerScore = 0;
+
+let innings = 1;
+
+let tossWon;
+const tossForm = document.getElementById('toss-form');
+const choiceForm = document.getElementById('choice-form');
+tossForm.addEventListener('submit',e => {
     e.preventDefault();
-    let userInput = form.userinput.value;
+
+    if(Math.floor(Math.random() * 2) == 1)
+        tossWon = 'User';
+    else
+        tossWon = 'Computer';
+    if(tossWon == 'User'){
+        document.getElementById('choice').style.display = 'flex';
+        
+    } else {
+        if(Math.floor(Math.random() * 2) == 1){
+            userRole = 'Batting';
+            computerRole = 'Bowling';
+        } else{
+            userRole = 'Bowling';
+            computerRole = 'Batting';
+        }
+        document.getElementById('game-area').style.display = 'flex';
+        document.getElementById('role').innerHTML = userRole;
+    }
+    
+    
+});
+choiceForm.addEventListener('submit',ev => {
+    ev.preventDefault();
+
+    userRole = choiceForm.choice.value;
+    document.getElementById('role').innerHTML = userRole;
+    if(userRole == 'Batting')
+        computerRole = 'Bowling';
+    else
+        computerRole = 'Batting';
+    document.getElementById('game-area').style.display = 'flex';
+});
+
+
+
+const handCricketForm = document.getElementById('hand-cricket');
+handCricketForm.addEventListener('submit', e => {
+    e.preventDefault();
+    let userInput = Number(handCricketForm.userinput.value);
     calculateScore(userInput);
 });
 
 function calculateScore(userInput){
-    let computerScore = Math.floor(Math.random() * 7);
-    let userScore = userInput;
+    let computerScoreTemp = Math.floor(Math.random() * 7);
+    let userScoreTemp = userInput;
 
-    if(user.role === 'batting'){
-        if(computerScore === userScore){
-            if(innings === 1){
-                user.role = 'bowling';
-                computer.role = 'batting';
+    console.log("You",userScoreTemp," Computer",computerScoreTemp);
+
+    if(userRole == 'Batting'){
+        if(computerScoreTemp == userScoreTemp){
+            if(innings == 1){
+                userRole = 'Bowling';
+                computerRole = 'Batting';
                 innings = 2;
             } else
-                gameOver = true;
-        } else if(userScore === 0)
-            user.score += computerScore;
-        else
-            user.score += userScore;
+                gameOver();
+        } else if(userScoreTemp == 0){
+            userScore += computerScoreTemp;
+            if(innings == 2 && userScore > computerScore)
+                gameOver();
+        } else{
+            userScore += userScoreTemp;
+            if(innings == 2 && userScore > computerScore)
+                gameOver();
+        }
     } else {
-        if(computerScore === userScore){
-            if(innings === 1){
-                user.role = 'batting';
-                computer.role = 'bowling';
+        if(computerScoreTemp == userScoreTemp){
+            if(innings == 1){
+                userRole = 'Batting';
+                computerRole = 'Bowling';
                 innings = 2;
             } else
-                gameOver = true;
-        } else if(computerScore === 0)
-            computer.score += userScore;
-        else
-            computer.score += computerScore;
+                gameOver();
+        } else if(computerScoreTemp == 0){
+            computerScore += userScoreTemp;
+            if(innings == 2 && computerScore > userScore){
+                gameOver();
+            }
+        } else{
+            computerScore += computerScoreTemp;
+            if(innings == 2 && computerScore > userScore)
+                    gameOver();
+        }
     }
 }
+
+function gameOver(){
+    console.log("Game over");
+    document.getElementById('game-area').style.display = 'none';
+    document.getElementById('score-area').style.display = 'flex';
+    document.getElementById('result').style.display = 'flex';
+    document.getElementById('user-score').innerHTML = 'Your Score: '+userScore;
+    document.getElementById('computer-score').innerHTML = "Computer's Score: "+computerScore;
+    if(userScore > computerScore)
+        document.getElementById('result').innerHTML = "You won";
+    else if(computerScore > userScore)
+        document.getElementById('result').innerHTML = "You lost";
+    else
+        document.getElementById('result').innerHTML = "Match Tied";
+}
+
+document.getElementById('role').innerHTML = userRole;
